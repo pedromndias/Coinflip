21212c811110a98574338b917fe26b69e6bb4bb1
Pedro Dias <pedromndias@gmail.com>
2020-03-23 16:37:04 +0100

    Event now works on Remix. Upgraded flipCoin(). Added getResultOfGame().

--- CoinflipProject/contracts/CoinFlip.sol
+++ CoinflipProject/contracts/CoinFlip.sol
@@ -5,14 +5,18 @@ contract CoinFlip is Ownable{
 
     // Let's create an event everytime someone "flips a coin", giving us his address:
     event coinFlipped(address);
-    /* And an event with how much was paid to the player:
-    event ammountPaidToPlayer(uint); CAN'T GET IT TO WORK */
+    // And an event with how much was paid to the player:
+    event ammountPaidToPlayer(uint);
 
     address public owner;
 
     uint private balance;
 
-    bool playerGotPaid; // For the event
+    // Variable for the result of the game:
+    uint resultOfGame;
+
+    // Variable for the ammount paid to the player:
+    uint valueToPlayer;
 
     // Function with some pseudo randomness, given by Filip:
     function random() public view returns(uint){
@@ -33,20 +37,25 @@ contract CoinFlip is Ownable{
         // The result of random() can either be 0 or 1. If it is 0 we send the funds (double) to the player. If it is 1, we keep it:
         if (now % 2 == 0){
             // send to the player:
-            uint valueToPlayer = 2*ethAmmount;
+            valueToPlayer = 2*ethAmmount;
             player.transfer(valueToPlayer);
-            return valueToPlayer;
-            // playerGotPaid = true; THIS WOULD BE FOR THE EVENT BUT CAN'T GET IT TO WORK. "unreachable code"
+            resultOfGame = valueToPlayer;
         }
         else{
         // keep the funds and update the balance:
         balance += msg.value;
+        resultOfGame = 0;
         }
 
         // Here we can sent out the event when someone uses the app:
         emit coinFlipped(msg.sender); // This is who called the function
-        /* Here we can sent out the event with the ammount paid to the player:
-        if (playerGotPaid = true) emit ammountPaidToPlayer(valueToPlayer); CAN'T GET IT TO WORK */
+        // Here we can sent out the event with the ammount paid to the player:
+        if (resultOfGame != 0) emit ammountPaidToPlayer(valueToPlayer);
+    }
+
+    // Let's write a function to send the data to the browser:
+    function getResultOfGame() public view returns (uint){
+        return resultOfGame;
     }
 
     // Let's write a function that gets the balance of an address:
